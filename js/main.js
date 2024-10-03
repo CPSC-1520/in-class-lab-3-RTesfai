24b7c0d0e855993c3dadbe689318c64c261a379d
Rachel Tesfai <rtesfai1@nait.ca>
2024-10-02 21:46:37 -0600

    first try

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 // Enter your code below.
+console.log("Javascript sucessfully linked");
+
+const resources = document.querySelector(".javascript-resources");
+const showResourcesButton = document.querySelector("#show-resources");
+
+showResourcesButton.addEventListener("click", (event)=>{
+    resources.classList.remove("d-none");
+});
+
+
+resources.addEventListener("mouseover", (event)=>{
+    console.log(event.target);
+    event.target.classList.add("fw-bold");
+});
+
+
+resources.addEventListener("mouseout", (event)=>{
+    event.target.classList.remove("fw-bold");
+});
+
+
+resources.addEventListener("click", (event)=>{
+    event.target.classList.add("fst-italic");
+});
+
+
